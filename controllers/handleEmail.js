2e22d8c2b75bb81cea0670a458e776173633c91b
KhallilB <khallil.bailey@gmail.com>
2019-10-22 17:49:05 -0700

    [add] sending emails

--- controllers/handleEmail.js
+++ controllers/handleEmail.js
@@ -1,17 +1,10 @@
 const EmailList = require('../models/EmailList');
-const nodemailer = require('nodemailer');
-const mg = require('nodemailer-mailgun-transport');
+const mailgun = require('mailgun-js');
 
-let auth = {
-  auth: {
-    api_key: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN
-  }
-};
-
-const nodemailerMailgun = nodemailer.createTransport(
-  mg(auth)
-);
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
+});
 
 const emailUser = async (req, res) => {
   let emailList = await new EmailList();
@@ -20,6 +13,24 @@ const emailUser = async (req, res) => {
   emailList.email = req.body.email;
 
   await emailList.save().then(email => {
+    let recipentEmail = email.email;
+    let recipentName = email.firstName;
+    const data = {
+      from: 'tonik.music.io@gmail.com',
+      to: recipentEmail,
+      subject: 'Here is some more info on Tonik',
+      text: `Hello ${recipentName},
+      
+        Thank you for showing interest in Tonik if you would like more information about Tonik, dont be afraid to email us at tonik.music.io@gmail.com. We will get back to you in a timely manner.
+        Thank you and have a wonderful day,
+        Your friends at Tonik
+        `
+    };
+
+    mg.messages().send(data, function(err, body) {
+      console.log(body);
+    });
+
     res.json({
       message: 'Thank you for showing intrest in Tonik!'
     });
